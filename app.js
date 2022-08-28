0a060793ed88a17b3972941bdbd4505625285cd0
BoFo <brunofazio466@gmail.com>
2022-08-28 12:40:29 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require ('path');
+const app = express() ;
+
+const publicPath = path.resolve(__dirname, './public');
+app.use( express.static(publicPath) ); 
+
+app.listen(3030,function() {
+    console.log('Corriendo');
+});
+
+app.get('/Index', (req,res) => {
+    res.sendFile(path.join(__dirname,'./views/index.html'));
+});
