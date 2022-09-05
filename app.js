106ff4ac75fb0742b5af674d1003b13e880baae3
BoFo <brunofazio466@gmail.com>
2022-09-05 18:13:08 -0300

    version 1 heroku

--- app.js
+++ app.js
@@ -10,30 +10,30 @@ app.listen(process.env.PORT || 3000, function() {
     console.log('Serevidor corriendo en el puerto 3000');
 });
 
-app.get('/', (req,res) => {
+app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname,'./views/index.html'));
 });
 
-app.get('/detalle-producto',(req,res) => {
-    res.sendFile(path.join(__dirname,'/views/detalle-producto.html'))
+app.get("/detalle-producto",(req,res) => {
+    res.sendFile(path.join(__dirname,'./views/detalle-producto.html'))
 });
 
-app.get('/productos',(req,res) => {
-    res.sendFile(path.join(__dirname,'/views/productos.html'))
+app.get("/productos",(req,res) => {
+    res.sendFile(path.join(__dirname,'./views/productos.html'))
 });
 
-app.get('/carrito-compras', (req,res) =>{
+app.get("/carrito-compras", (req,res) =>{
     res.sendFile(path.resolve(__dirname, './views/carrito.html'));  // Permite enviar un archivo HTML
 });
 
-app.get('/login', (req,res) =>{
+app.get("/login", (req,res) =>{
     res.sendFile(path.resolve(__dirname, './views/login.html'));  // Permite enviar un archivo HTML
 });
 
-app.get('/registro', (req,res) =>{
+app.get("/registro", (req,res) =>{
     res.sendFile(path.resolve(__dirname, './views/registro.html'));  // Permite enviar un archivo HTML
 });
 
-app.get('/rec_contra', (req,res) =>{
+app.get("/rec_contra", (req,res) =>{
     res.sendFile(path.resolve(__dirname, './views/rec_contra.html'));  // Permite enviar un archivo HTML
 });
