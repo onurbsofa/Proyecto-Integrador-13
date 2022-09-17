ab494e59e8f31f5bf3ccf854ea90016ffc144c4c
Martin Leibovici <martinleibovici@martin.leibovici>
2022-09-17 18:42:23 +1000

    Implementacion MVC + ejs y vista partials del header

--- app.js
+++ app.js
@@ -1,39 +1,23 @@
+const todosRoutes = require('./src/routes/todosRoutes')
 const express = require('express');
 const path = require ('path');
-const app = express() ;
+const app = express();
 
 const publicPath = path.resolve(__dirname, './public');
-app.use( express.static(publicPath) ); 
+app.use( express.static(publicPath) );
+app.set('view engine', 'ejs'); 
 
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Serevidor corriendo en el puerto 3000');
 });
 
-app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname,'./views/index.html'));
-});
+app.use("/", todosRoutes);
+app.use("/detalle-producto", todosRoutes);
+app.use("/productos", todosRoutes);
+app.use("/carrito-compras", todosRoutes);
+app.use("/login", todosRoutes);
+app.use("/registro", todosRoutes);
+app.use("/rec_contra", todosRoutes);
 
-app.get("/detalle-producto",(req,res) => {
-    res.sendFile(path.join(__dirname,'./views/detalle-producto.html'))
-});
 
-app.get("/productos",(req,res) => {
-    res.sendFile(path.join(__dirname,'./views/productos.html'))
-});
-
-app.get("/carrito-compras", (req,res) =>{
-    res.sendFile(path.resolve(__dirname, './views/carrito.html'));  // Permite enviar un archivo HTML
-});
-
-app.get("/login", (req,res) =>{
-    res.sendFile(path.resolve(__dirname, './views/login.html'));  // Permite enviar un archivo HTML
-});
-
-app.get("/registro", (req,res) =>{
-    res.sendFile(path.resolve(__dirname, './views/registro.html'));  // Permite enviar un archivo HTML
-});
-
-app.get("/rec_contra", (req,res) =>{
-    res.sendFile(path.resolve(__dirname, './views/rec_contra.html'));  // Permite enviar un archivo HTML
-});
---
+++ src/controllers/todosControllers.js
@@ -0,0 +1,34 @@
+const controllers = {
+
+
+index : (req,res) => {
+    res.render('index');
+},
+
+detalleProducto : (req,res) => {
+    res.render('detalle-producto');
+},
+
+productos : (req,res) => {
+    res.render('productos');
+},
+
+carritoCompras : (req,res) =>{
+    res.render('carrito');  
+},
+
+login :  (req,res) =>{
+    res.render('login');  
+},
+
+registro : (req,res) =>{
+    res.render('registro');               
+},
+
+recContra : (req,res) =>{
+    res.render('rec_contra');            
+}
+
+}
+
+module.exports = controllers;
