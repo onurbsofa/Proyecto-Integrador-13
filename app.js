341ccfd13572a89adf1b1ccb10a650939bbd5cec
BoFo <brunofazio466@gmail.com>
2022-09-30 11:44:34 -0300

    views en el src

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const express = require('express');
 const path = require ('path');
 const app = express();
 
-const publicPath = path.resolve(__dirname, './public');
-app.use( express.static(publicPath) );
+
+app.use( express.static('public') );
 app.set('view engine', 'ejs'); 
+app.set('views',path.join(__dirname,'./src/views'));
 
 
 app.listen(process.env.PORT || 3000, function() {
