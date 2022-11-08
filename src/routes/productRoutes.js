421f787e86598f05001ce9a699c4a99b12441d33
BoFo <brunofazio466@gmail.com>
2022-11-08 00:52:49 -0300

    sprint 5

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -4,6 +4,8 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer'); //Multer
 const path = require('path');
+const { check, validationResult, body} = require('express-validator');
+const usuarioMiddleware = require('../middlewares/usuarioMiddleware');
 
 //variable almacenamiento Multer
 const multerDiskStorage = multer.diskStorage({
@@ -23,8 +25,8 @@ router.get('/listado-de-productos', productControllers.listadoProductos);
 router.get('/detalle-producto/:id', productControllers.detalleProducto);
 
 //Crear producto
-router.get('/nuevo-producto', productControllers.nuevoProducto);
-router.post('/nuevo-producto', uploadFile.single('imagen') , productControllers.crear); 
+router.get('/nuevo-producto',usuarioMiddleware, productControllers.nuevoProducto);
+router.post('/nuevo-producto',usuarioMiddleware, uploadFile.single('imagen') , productControllers.crear); 
 
 //Editar Producto
 router.get('/editar-producto/:id', productControllers.editarProducto);
