18954c99950334d0f2a77223f98d517aa820040c
Martin Leibovici <martinleibovici@martin.leibovici>
2022-11-17 12:16:42 +1100

    heroku config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "tostao",
+    "password": "3!@@xn7cD6Uq6A@",
+    "database": "tostao_db",
+    "host": "mysql-tostao.alwaysdata.net",
     "dialect": "mysql"
   }
 }
